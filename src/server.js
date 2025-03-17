9d2a40a761fff8a04c9589f1774fedb0dfeae520
yongbin kim <kyb@agencygo.net>
2025-03-17 10:10:29 +0900

    ✨ Socket.io 통합: 서버 설정 및 클라이언트 스크립트 추가

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import http from "http";
+import { Server } from "socket.io";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -8,12 +10,19 @@ const port = 3000;
 
 const app = express();
 
-app.use("public", express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.listen(port, () => {
-  console.log("Server is running on port 3000");
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
+
+wsServer.on("connection", (socket) => {
+  console.log("A user connected");
+});
+
+httpServer.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
